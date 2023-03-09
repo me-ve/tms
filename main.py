ba612b53ffe868ac6c9be861d709f1feb0a019b5
me-ve <68286584+me-ve@users.noreply.github.com>
2023-03-10 00:36:47 +0100

    whitespaces

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ def load_instructions(filename) -> tuple[chr, str, dict]:
         states[name][symbol_in]["next"] = next_state
         states[name][symbol_in]["direction"] = direction.upper()
     return null_char, start_state, states
+
 def load_tape(filename) -> list[chr]:
     tape = []
     with open(filename, "r") as file:
@@ -32,9 +33,11 @@ def load_tape(filename) -> list[chr]:
     tape = list(lines[0])
     head_pos = int(lines[1])
     return tape, head_pos
+
 def save_tape(filename, tape, head):
     with open(filename, "w") as file:
         file.write("".join(tape)+"\n"+str(head))
+
 def draw_tape(tape:list[chr], null_char:chr, head:int, width:int) -> None:
     n = len(tape)
     min_cell = head - width // 2
@@ -51,14 +54,17 @@ def draw_tape(tape:list[chr], null_char:chr, head:int, width:int) -> None:
         else:
             res += f"| {tape[i]} "
     print(res + "|" + "\n" + under_res + " ")
+
 def get_current_symbol(tape, head, null_char):
     return tape[head] if head in range(len(tape)) else null_char
+
 def fetch_state(name, states, symbol) -> dict:
     if name not in states:
         return {}
     if symbol not in states[name]:
         return {}
     return states[name][symbol]
+
 def adjust_tape(tape:list[chr], head:int, null_char:chr) -> list[chr]:
     if head not in range(len(tape)):
         if head < 0:
@@ -69,6 +75,7 @@ def adjust_tape(tape:list[chr], head:int, null_char:chr) -> list[chr]:
             while head >= len(tape):
                 tape.append(null_char)
     return tape, head
+
 def execute_state(tape:list[chr], state_properties:dict, head:int) -> tuple[list, int, str]:
     new_symbol = state_properties["out"]
     direction = state_properties["direction"]
@@ -78,6 +85,7 @@ def execute_state(tape:list[chr], state_properties:dict, head:int) -> tuple[list
     d_head = 1 if direction == "R" else -1
     head += d_head
     return tape, head, next_state
+
 def process_state(tape:list, states:dict, current_state:str, head:int, null_char:chr) -> bool:
     current_symbol = get_current_symbol(tape, head, null_char)
     state_properties = fetch_state(current_state, states, current_symbol)
@@ -87,6 +95,7 @@ def process_state(tape:list, states:dict, current_state:str, head:int, null_char
         tape, head, next_state = execute_state(tape, state_properties, head)
         tape, head = adjust_tape(tape, head, null_char)
     return state_exists, head, next_state
+
 def main_loop(states_file:str, tape_file:str):
     sleep_time = 0
     tape_display_width = 16
@@ -98,7 +107,12 @@ def main_loop(states_file:str, tape_file:str):
     print(f"Head: {head}")
     print("Available states:")
     for name in states:
-        print(name, "->", states[name])
+        print(f"{name}:")
+        for symbol_in in states[name]:
+            symbol_out = states[name][symbol_in]["out"]
+            next_state = states[name][symbol_in]["next"]
+            direction = states[name][symbol_in]["direction"]
+            print(f"\t{symbol_in}: output: {symbol_out}, next state: {next_state}, direction: {direction}")
     status = True
     ticks = 0
     while status:
@@ -114,8 +128,7 @@ def main_loop(states_file:str, tape_file:str):
 def main():
     if len(sys.argv) != 3:
         print(f"Using: python {sys.argv[0]} <state-file> <tape-file>")
-        return 1
+        exit(1)
     main_loop(sys.argv[1], sys.argv[2])
-    return 0
 
 main()
